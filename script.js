47e64a8d8c83a875b873b65bed7f481572b7bf7a
Alex McDaniel <amcdaniel186@gmail.com>
2018-11-06 10:14:19 -0500

    update1

---
+++ script.js
@@ -0,0 +1,15 @@
+// examples to initialize jQuery in javascript files
+
+// function executeWhenLoaded () {
+//   alert("DOM is loaded");
+// };
+
+// $(document).ready(executeWhenLoaded);
+
+// $(document).ready(function() {
+//   alert("DOM is loaded");
+// });
+
+// $(function() {
+//   alert("DOM is loaded");
+// });
